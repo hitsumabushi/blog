11cb63e8765a741236ae8f907c183a45d9204042
hitsumabushi <053023.math@gmail.com>
2014-12-31 17:43:26 +0900

    add comment to build script

--- drone.sh
+++ drone.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
-
 set -x
 
+# resolv dependency
 pip install -r requirements.txt --use-mirrors
 
+# build html
 make clean
 make html
+
+# checkout branch
 git checkout -b gh-pages
 
+# copy output to top of repository
 cp -r output/* .
 
+# Remove unpublish files
 rm -r content
 rm -r output
 rm *.sh
@@ -19,8 +24,9 @@ rm requirements.txt
 rm .gitignore
 rm -rf themes/*
 
+# Git push
 git add -A .
-git commit -m "build by drone.io"
+git commit -m "Build by drone.io"
 git remote set-url origin git@github.com:hitsumabushi/blog.git
-git push -f origin gh-pages
+git push -f origin gh-pages  # forced push
 
